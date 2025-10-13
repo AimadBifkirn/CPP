fb41725e6b686c7e9af7dbb1710d9a893c1d86f4
AimadBifkirn <aimadbifkirn@gmail.com>
2025-10-13 15:08:19 +0100

    added

--- CPP_02/ex01/Fixed.cpp
+++ CPP_02/ex01/Fixed.cpp
@@ -31,13 +31,11 @@ Fixed::~Fixed ()
 
 int Fixed::getRawBits(void) const
 {
-	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->fp_value);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	// std::cout << "SetRawBits member function called" << std::endl;
 	this->fp_value = raw;
 }
 
