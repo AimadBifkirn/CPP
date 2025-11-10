191283c8987d26972b6e3f80e8fe503b1bb7753f
AimadBifkirn <aimadbifkirn@gmail.com>
2025-11-10 09:05:32 +0100

    still need to implement make

--- CPP_05/ex03/Intern.cpp
+++ CPP_05/ex03/Intern.cpp
@@ -7,3 +7,17 @@ Intern::Intern ()
 Intern::Intern (const Intern &other)
 {
 }
+
+Intern &Intern::operator= (const Intern &obj)
+{
+	return *this;
+}
+
+Intern::~Intern ()
+{
+}
+
+AForm *Intern::makeForm(const std::string &name, const std::string &target) const
+{
+
+}
