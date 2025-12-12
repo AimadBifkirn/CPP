eccfa93894efcd00a6fef13da4c4561223f3bb7c
AimadBifkirn <aimadbifkirn@gmail.com>
2025-12-12 17:05:02 +0100

    a small change in main (ex01)

--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -2,16 +2,14 @@
 
 int main()
 {
-
-	Serializer s;
     Data d = {1337, 'A'};
 
-	uintptr_t u =  s.serialize(&d);
+	uintptr_t u =  Serializer::serialize(&d);
 	std::cout << &d << std::endl;
 	std::cout << "int i = " << d.i << ", char c = " << d.c << std::endl;
 	std::cout << u << std::endl;
 	
-	Data *res = s.deserialize(u);
+	Data *res = Serializer::deserialize(u);
 	std::cout << res << std::endl;
 	std::cout << "int i = " << res->i << ", char c = " << res->c << std::endl;
     return 0;
