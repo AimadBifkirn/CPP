186ce05537fcb8a64a8ae5706269b11dc5ab18c0
AimadBifkirn <aimadbifkirn@gmail.com>
2026-01-12 16:19:55 +0100

    finished e

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -4,16 +4,59 @@
 
 template <typename T>
 class Array {
-private:
-    T* elements;
-    unsigned int size;
-public:
-    Array();
-    Array(unsigned int n);
-    Array(const Array& other);
-    Array& operator=(const Array& other);
-    ~Array();
-    T& operator[](unsigned int index);
-    const T& operator[](unsigned int index) const;
-    unsigned int getSize() const;
+    private:
+        T* elements;
+        unsigned int _size;
+    public:
+        Array() : elements(NULL), _size(0) {}
+
+        Array(unsigned int n) {
+            _size = n;
+            elements = new T[n]();
+        }
+
+        Array(const Array& other)
+        {
+            if (other._size == 0)
+            {
+                this->elements = NULL;
+                this->_size = 0;
+                return;
+            }
+            this->elements = new T[other._size]();
+            this->_size = other._size;
+            for (unsigned int i = 0; i < other._size; i++) {
+                this->elements[i] = other.elements[i];
+            }
+        }
+
+        Array& operator=(const Array& other)
+        {
+            if (this == &other)
+                return *this;
+            delete[] this->elements;
+            if (other._size == 0)
+            {
+                this->elements = NULL;
+                this->_size = 0;
+                return *this;
+            }
+            this->elements = new T[other._size]();
+            this->_size = other._size;
+            for (unsigned int i = 0; i < other._size; i++) {
+                this->elements[i] = other.elements[i];
+            }
+            return *this;
+        }
+
+        ~Array() { delete[] elements; }
+
+        T& operator[](unsigned int index)
+        {
+            if (index >= _size)
+                throw std::out_of_range("Index out of bounds");
+            return elements[index];
+        }
+
+        unsigned int size() const { return _size; }
 };
---
+++ CPP_07/ex02/main.cpp
@@ -0,0 +1,22 @@
+#include "Array.hpp"
+
+int main ()
+{
+    Array<int> arr(5);
+
+    for (unsigned int i = 0; i < arr.size(); i++) {
+        arr[i] = i * 10;
+    }
+
+    for (unsigned int i = 0; i < arr.size(); i++) {
+        std::cout << "Element at index " << i << ": " << arr[i] << std::endl;
+    }
+
+    try {
+        std::cout << arr[10] << std::endl; // This should throw an exception
+    } catch (const std::out_of_range& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
