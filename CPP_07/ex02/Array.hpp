d8f426400459fc5c81a96603385e4892733d3aef
AimadBifkirn <aimadbifkirn@gmail.com>
2026-01-12 14:16:17 +0100

    started CPP07/ex02

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <iostream>
+
+template <typename T>
+class Array {
+private:
+    T* elements;
+    unsigned int size;
+public:
+    Array();
+    Array(unsigned int n);
+    Array(const Array& other);
+    Array& operator=(const Array& other);
+    ~Array();
+    T& operator[](unsigned int index);
+    const T& operator[](unsigned int index) const;
+    unsigned int getSize() const;
+};
