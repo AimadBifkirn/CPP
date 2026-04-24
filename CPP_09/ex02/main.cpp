20483c999978972cb6d8ecdf6564bd9d3b846147
AimadBifkirn <aimadbifkirn@gmail.com>
2026-04-24 10:06:20 +0100

    added the tim,e

--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -47,24 +47,10 @@ int main (int argc, char **argv)
     }        
     std::cout << std::endl;
     std::deque<int> deq(vect.begin(), vect.end());
-    // for (int i = 1; i < argc; i++)
-    // {
-    //     std::istringstream iss(argv[i]);
-    //     int num;
-    //     if (!(iss >> num))
-    //     {
-    //         std::cerr << "Error: Invalid number '" << argv[i] << "'" << std::endl;
-    //         return 1;
-    //     }
-    //     deq.push_back(num);
-    // }
     clock_t start_D = clock();
     pmergeMe.sortDeque(deq);
     clock_t end_D = clock();
-    // for (size_t i = 0; i < deq.size(); i++)
-    //     std::cout << deq[i] << " ";
-    // std::cout << std::endl;
-    std::cout << "Time to process a range of  " << vect.size() << " elements with std::vector : " << (double)(end_V - start_V) / CLOCKS_PER_SEC * 1000 << " us" << std::endl;
-    std::cout << "Time to process a range of  " << deq.size() << " elements with std::deque : " << (double)(end_D - start_D) / CLOCKS_PER_SEC * 1000 << " us" << std::endl;
+    std::cout << "Time to process a range of  " << vect.size() << " elements with std::vector : " << static_cast<double>(end_V - start_V) * 1000000 / CLOCKS_PER_SEC << " us" << std::endl;
+    std::cout << "Time to process a range of  " << deq.size() << " elements with std::deque : " << static_cast<double>(end_D - start_D) * 1000000 / CLOCKS_PER_SEC << " us" << std::endl;
     return 0;
 }
