a243a45e7a2e44137488856c8c74d2b558d48809
AimadBifkirn <aimadbifkirn@gmail.com>
2026-04-29 11:01:27 +0100

    modified the main

--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -8,6 +8,7 @@ int main (int argc, char **argv)
         std::cerr << "Usage: " << argv[0] << " <numbers...>" << std::endl;
         return 1;
     }
+
     std::vector<int> vect;
     for (int i = 1; i < argc; i++)
     {
@@ -20,36 +21,27 @@ int main (int argc, char **argv)
         }
         vect.push_back(num);
     }
+
     std::cout << "Before: ";
     for (size_t i = 0; i < vect.size(); i++)
-    {
         std::cout << vect[i] << " ";
-        if ((i == 4 && vect.size() > 5))
-        {
-            std::cout << "[...] ";
-            break;
-        }
-    }
     std::cout << std::endl;
+
     PmergeMe pmergeMe;
     clock_t start_V = clock();
     pmergeMe.sortVector(vect);
     clock_t end_V = clock();
+
     std::cout << "After: ";
     for (size_t i = 0; i < vect.size(); i++)
-    {
-        std::cout << vect[i] << " ";
-        if ((i == 4 && vect.size() > 5))
-        {
-            std::cout << "[...] ";
-            break;
-        }
-    }        
+        std::cout << vect[i] << " ";    
     std::cout << std::endl;
+
     std::deque<int> deq(vect.begin(), vect.end());
     clock_t start_D = clock();
     pmergeMe.sortDeque(deq);
     clock_t end_D = clock();
+
     std::cout << "Time to process a range of  " << vect.size() << " elements with std::vector : " << static_cast<double>(end_V - start_V) * 1000000 / CLOCKS_PER_SEC << " us" << std::endl;
     std::cout << "Time to process a range of  " << deq.size() << " elements with std::deque : " << static_cast<double>(end_D - start_D) * 1000000 / CLOCKS_PER_SEC << " us" << std::endl;
     return 0;
